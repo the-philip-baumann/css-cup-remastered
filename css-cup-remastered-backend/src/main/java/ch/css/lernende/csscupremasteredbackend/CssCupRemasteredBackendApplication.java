3593a2af711482bafe63beebaa26a9c6e2ef4409
the-philip-baumann <philip.baumann@hispeed.ch>
2021-03-24 12:31:41 +0100

    Setup modules

---
+++ css-cup-remastered-backend/src/main/java/ch/css/lernende/csscupremasteredbackend/CssCupRemasteredBackendApplication.java
@@ -0,0 +1,13 @@
+package ch.css.lernende.csscupremasteredbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CssCupRemasteredBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CssCupRemasteredBackendApplication.class, args);
+	}
+
+}
