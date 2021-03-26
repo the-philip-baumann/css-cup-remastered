254a43732f7dec3eb72bb3838f65158fc59bc867
the-philip-baumann <philip.baumann@hispeed.ch>
2021-03-26 15:43:25 +0100

    [Frontend] Header implementation

--- css-cup-remastered-frontend/src/app/app-routing.module.ts
+++ css-cup-remastered-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: 'home',
+  //   pathMatch: 'full',
+  //   component: null,
+  // },
+  // {
+  //   path: 'administration',
+  //   component: null
+  // },
+  // {
+  //   path: 'profile',
+  //   component: null
+  // },
+  // {
+  //   path: 'uebersicht',
+  //   component: null
+  // },
+  // {
+  //   path: 'begegnungen',
+  //   component: null,
+  // }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
