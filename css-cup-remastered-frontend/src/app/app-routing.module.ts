747458dc3d81a73a6dc9acc91a76acc7d3dc52d5
the-philip-baumann <philip.baumann@hispeed.ch>
2021-03-26 21:19:15 +0100

    [Frontend] Team Uebersicht basic implementation

--- css-cup-remastered-frontend/src/app/app-routing.module.ts
+++ css-cup-remastered-frontend/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {UebersichtComponent} from "./uebersicht/uebersicht.component";
 
 const routes: Routes = [
   // {
@@ -15,10 +16,11 @@ const routes: Routes = [
   //   path: 'profile',
   //   component: null
   // },
-  // {
-  //   path: 'uebersicht',
-  //   component: null
-  // },
+  {
+    path: 'uebersicht',
+    component: UebersichtComponent,
+
+  },
   // {
   //   path: 'begegnungen',
   //   component: null,
