aacaa64979c07f19527e9e44dc8048555ea33187
the-philip-baumann <philip.baumann@hispeed.ch>
2021-06-03 11:34:29 +0200

    [Frontend] Implement register with responsivness

--- css-cup-remastered-frontend/src/app/app.module.ts
+++ css-cup-remastered-frontend/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { CreateTeamComponent } from './create-team/create-team.component';
 import {FormsModule} from "@angular/forms";
 import { DisciplineSwitchButtonComponent } from './util/discipline-switch-button/discipline-switch-button.component';
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { HomeComponent } from './home/home.component';
     CreateTeamComponent,
     DisciplineSwitchButtonComponent,
     HomeComponent,
+    RegisterComponent,
   ],
     imports: [
         BrowserModule,
